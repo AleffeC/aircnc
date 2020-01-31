7031199f7ba03d723921bba209fa627d55306bec
aleffec <aleffecodignole@hotmail.com>
2020-01-31 09:25:13 -0300

    Conexao com Mongo DB e preparação da model e controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello Word');
+mongoose.connect("mongodb+srv://aleffe:aleffe@omnistack-7pvf4.mongodb.net/semana09?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology:  true,
 });
+//req.query = para filtros
+//req.params = para edição e delete 
+// req.body = para criação e edição de registros
+//Sequelize = utilizar com sql server 
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
+
