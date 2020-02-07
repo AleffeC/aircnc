2876ff84c2fd1b5890ba38063579cc0d2144a408
aleffec <aleffecodignole@hotmail.com>
2020-02-07 10:52:07 -0300

    Iniciando API/useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
 
 import logo from './assets/logo.svg';
 
 //incluir codigo javascript dentro de html sempre com chaves = {}
 
 function App() {
+
+  const [email, setEmail] = useState(''); // setEmail atualizara o estado da variavel email
+
+  function HandleSubmit(event){
+    event.preventDefault();
+
+    console.log(email);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -15,12 +25,14 @@ function App() {
             Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
           </p>
 
-          <form>
+          <form onSubmit={HandleSubmit}>
             <label htmlFor="email">E-MAIL *</label>
               <input 
                 type="email" 
                 id="email" 
                 placeholder="só vai"
+                value={email}
+                onChange={event => setEmail(event.target.value)} /*função arrow function*/
             />
             <button type="submit" className="btn" >Entrar</button>
           </form>
