0638576f74b7f1057b097fce5beafbdbd027c139
aleffec <aleffecodignole@hotmail.com>
2020-02-07 10:24:49 -0300

    Tela inicial do sistema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
+//incluir codigo javascript dentro de html sempre com chaves = {}
+
 function App() {
   return (
-    <h1>Hello Word</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+        
+      <div className="content">
+          <p>
+            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+          </p>
+
+          <form>
+            <label htmlFor="email">E-MAIL *</label>
+              <input 
+                type="email" 
+                id="email" 
+                placeholder="só vai"
+            />
+            <button type="submit" className="btn" >Entrar</button>
+          </form>
+      </div>
+    </div>
+
   );
 }
 
